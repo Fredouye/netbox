069b5b65ce242380a7e43c842b485738e36a6d01
Fredouye <frederic.mangeant@gmail.com>
2023-12-19 19:04:32 +0100

    Update new_site.py

--- scripts/new_site.py
+++ scripts/new_site.py
@@ -8,31 +8,38 @@ from dcim.models import Device, DeviceRole, DeviceType, Site
 class NewBranchScript(Script):
 
     class Meta:
-        name = "Nouveau site"
-        description = "Provisionne un nouveau site"
+        name = "New Branch"
+        description = "Provision a new branch site"
 
     site_name = StringVar(
-        description="Non du nouveau site"
+        description="Name of the new site"
     )
     switch_count = IntegerVar(
-        description="Nombre de switches à créer"
+        description="Number of access switches to create"
     )
     switch_model = ObjectVar(
-        description="Modèle de switch",
+        description="Access switch model",
         model=DeviceType
     )
     router_count = IntegerVar(
-        description="Nombre de firewalls à créer"
+        description="Number of routers to create"
     )
     router_model = ObjectVar(
-        description="Modèle de firewall",
+        description="Router model",
+        model=DeviceType
+    )
+    ap_count = IntegerVar(
+        description="Number of APs to create"
+    )
+    ap_model = ObjectVar(
+        description="AP model",
         model=DeviceType
     )
     server_count = IntegerVar(
-        description="Nombre de serveurs à créer"
+        description="Number of servers to create"
     )
     server_model = ObjectVar(
-        description="Modèle de serveur",
+        description="Server model",
         model=DeviceType
     )
 
@@ -48,11 +55,11 @@ class NewBranchScript(Script):
         self.log_success(f"Created new site: {site}")
 
         # Create access switches
-        switch_role = DeviceRole.objects.get(name='Switch')
+        switch_role = DeviceRole.objects.get(name='Access Switch')
         for i in range(1, data['switch_count'] + 1):
             switch = Device(
                 device_type=data['switch_model'],
-                name=f'{site.slug.upper()}-SWITCH-{i}',
+                name=f'{site.slug.upper()}-SW-{i}',
                 site=site,
                 status=DeviceStatusChoices.STATUS_PLANNED,
                 device_role=switch_role
@@ -61,30 +68,43 @@ class NewBranchScript(Script):
             self.log_success(f"Created new switch: {switch}")
 
         # Create routers
-        router_role = DeviceRole.objects.get(name='Firewall')
+        router_role = DeviceRole.objects.get(name='WAN Router')
         for i in range(1, data['router_count'] + 1):
             router = Device(
                 device_type=data['router_model'],
-                name=f'{site.slug.upper()}-FIREWALL-{i}',
+                name=f'{site.slug.upper()}-RTR-{i}',
                 site=site,
                 status=DeviceStatusChoices.STATUS_PLANNED,
                 device_role=router_role
             )
             router.save()
             self.log_success(f"Created new router: {router}")
-       
+
+        # Create APs
+        ap_role = DeviceRole.objects.get(name='Wireless AP')
+        for i in range(1, data['ap_count'] + 1):
+            ap = Device(
+                device_type=data['ap_model'],
+                name=f'{site.slug.upper()}-AP-{i}',
+                site=site,
+                status=DeviceStatusChoices.STATUS_PLANNED,
+                device_role=ap_role
+            )
+            ap.save()
+            self.log_success(f"Created new AP: {router}")
+        
         # Create Servers
-        server_role = DeviceRole.objects.get(name='ESXi')
+        server_role = DeviceRole.objects.get(name='vSphere')
         for i in range(1, data['server_count'] + 1):
             server = Device(
                 device_type=data['server_model'],
-                name=f'{site.slug.upper()}-ESX-{i}',
+                name=f'{site.slug.upper()}-VSP-{i}',
                 site=site,
                 status=DeviceStatusChoices.STATUS_PLANNED,
                 device_role=server_role
             )
             server.save()
-            self.log_success(f"Created new server: {server}")
+            self.log_success(f"Created new server: {router}")
 
         # Generate a CSV table of new devices
         output = [
